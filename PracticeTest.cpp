729fdbd35725dd7e48236d46544d383898ef2144
dlhazelton <dlhazelton@mail.csuchico.edu>
2020-11-16 22:47:41 -0800

    x13 submission, make clean used

--- PracticeTest.cpp
+++ PracticeTest.cpp
@@ -14,9 +14,65 @@ class PracticeTest : public ::testing::Test
 		virtual void TearDown(){} //clean up after each test, (before destructor)
 };
 
-TEST(PracticeTest, is_simple_palindrome)
+// Examples of a Happy Test Case for isPalindrome
+TEST(PracticeTest, is_not_simple_palindrome)
 {
     Practice obj;
     bool actual = obj.isPalindrome("Not a palindrome");
+    ASSERT_FALSE(actual);
+}
+
+TEST(PracticeTest, is_simple_palindrome)
+{
+    Practice obj;
+    bool actual = obj.isPalindrome("Racecar");
     ASSERT_TRUE(actual);
+}
+
+// Examples of a Sad Test Case for isPalindrome
+TEST(PracticeTest, symbol_not_palindrome)
+{
+    Practice obj;
+    bool actual = obj.isPalindrome(";_+");
+    ASSERT_FALSE(actual);
+}
+
+TEST(PracticeTest, empty_string_palindrome)
+{
+    Practice obj;
+    bool actual = obj.isPalindrome("");
+    ASSERT_TRUE(actual);
+}
+
+// Examples of a Happy Test Case for sortDescending
+TEST(PracticeTest, is_now_descending)
+{
+    Practice obj;
+		int first = 3;
+		int second = 2;
+		int third = 1;
+    obj.sortDescending(first, second, third);
+    ASSERT_GT(first, second);
+		ASSERT_GT(second, third);
+}
+
+TEST(PracticeTest, is_not_descending)
+{
+    Practice obj;
+		int first = 2;
+		int second = 3;
+		int third = 1;
+		obj.sortDescending(first, second, third);
+    ASSERT_GT(first, second);
+		ASSERT_GT(second, third);
+}
+
+// Examples of a Sad Test Case for sortDescending
+TEST(PracticeTest, empty_descending)
+{
+    Practice obj;
+		int first;
+		int second;
+		int third;
+    ASSERT_NO_THROW(obj.sortDescending(first, second, third));
 }
